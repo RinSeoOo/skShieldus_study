12613e507cb43731d64d6fcb3c7c52261224be26
문서영 <msy042195@gmail.com>
2025-11-26 13:17:25 +0900

    저장소 정리

--- 보안 실무 과제/모의해킹/python/ssrf_04.py
+++
@@ -1,8 +0,0 @@
-import requests
-import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning) # 인증서 없을 때 오류 안보이게 함
-
-url = "https://lab.eqst.co.kr:8099/ssrf_2/imageview.php?host=127.0.0.1"
-h = {"Host":"lab.eqst.co.kr:8099",
-     "Content-Type": "application/x-www-form-urlencoded"}
-c = {"PHPSESSID":"905304195bf47b2df4c5264a0e41068a"}
